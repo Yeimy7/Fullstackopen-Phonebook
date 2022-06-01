e725fd5e9aa0edca56165ad60803fa4dcd86ba2a
Yeim <brielalica@gmail.com>
2022-06-01 18:44:04 -0400

    Part2 - c.-d. Getting and altering data from server - Exercises 2.11., 2.15.-2.18. done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
+import personsService from "./services/persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [nameSearch, setNameSearch] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [nameFilter, setNameFilter] = useState(persons)
 
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleNameFilter = (e) => {
     setNameSearch(e.target.value)
-    if (e.target.value === '') {
-      setNameFilter(persons)
-    }
-    else {
-      setNameFilter(persons.filter(person => (person.name.toLocaleLowerCase()).match(nameSearch.toLowerCase())))
-    }
   }
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -35,17 +33,40 @@ function App() {
     e.preventDefault()
     const existName = persons.some(person => person.name === newName)
     if (!existName) {
-      setPersons([...persons, { name: newName, number: newNumber }])
-      setNewName('')
-      setNewNumber('')
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      personsService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
     else {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(p => p.name === newName)
+        const changedPerson = { ...person, number: newNumber }
+        personsService
+          .update(person.id, changedPerson)
+          .then(changed => {
+            setPersons(persons.map(person => person.name !== newName ? person : changed))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
     }
-
-
   }
 
+  const deleteName = (id) => {
+    personsService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
+  }
   return (
     <div>
       <h2>Phonebook</h2>
@@ -53,7 +74,7 @@ function App() {
       <h2>add a new</h2>
       <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons nameFilter={nameFilter} />
+      <Persons nameFilter={persons.filter(person => (person.name.toLocaleLowerCase()).match(nameSearch.toLowerCase()))} deleteName={deleteName} />
     </div>
   );
 }
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export const Persons = ({nameFilter}) => {
+export const Persons = ({ nameFilter, deleteName }) => {
+  const handleDelete=(id, name)=>{
+    if(window.confirm(`Delete ${name}?`)){
+      deleteName(id)
+    }
+  }
   return (
     <div>
-    {nameFilter.map(person => <p key={person.name}>{`${person.name} ${person.number}`}</p>)}
-  </div>
+      {nameFilter.map(person => <p key={person.id}>{`${person.name} ${person.number}`} <button onClick={()=>handleDelete(person.id, person.name )}>delete</button></p>)}
+    </div>
   )
 }
